47d16943ea9b2e17a8924787a581350cea83fbbf
Juan Antonio Navarro Pérez <juannavarroperez@gmail.com>
2013-08-22 17:33:26 +0100

    added example

---
+++ example1.py
@@ -0,0 +1,13 @@
+def SumSquares(n):
+    """Sum squares of numbers from 1 to n"""
+    sum = 0
+    for i in range(1, n + 1):
+      sum = sum + i ** 2
+    return sum
+
+print(SumSquares(1))
+print(SumSquares(2))
+print(SumSquares(3))
+print(SumSquares(4))
+print(SumSquares(5))
+print(SumSquares(6))
