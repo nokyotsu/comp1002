6e2691caa1282f9fcfca58eae5d7742a0c7a767e
Juan Antonio Navarro Pérez <juannavarroperez@gmail.com>
2013-09-24 09:52:29 +0100

    tabs to spaces

--- insertsort.py
+++ insertsort.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 def InsertionSort(A):
-	"""Sort elements in array A"""
-	for j in range(1, len(A)):
-		x = A[j]
-		i = j
-		while 0 < i and x < A[i - 1]:
-			A[i] = A[i - 1]
-			i = i - 1
-		A[i] = x
-	return A
+    """Sort elements in array A"""
+    for j in range(1, len(A)):
+        x = A[j]
+        i = j
+        while 0 < i and x < A[i - 1]:
+            A[i] = A[i - 1]
+            i = i - 1
+        A[i] = x
+    return A
 
 A = [53, 3, 42, 82, 2, 14]
 print(A[2])
