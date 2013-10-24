8449b668b2690f99a6a0994c509a0f72c9d39e5d
Juan Antonio Navarro Pérez <juannavarroperez@gmail.com>
2013-10-24 13:51:43 +0100

    added heap delete

--- heap.py
+++ heap.py
@@ -9,9 +9,27 @@ def heap_insert(H, x):
       break
     (H[p], H[i]) = (H[i], H[p])
     i = p
+    
+def heap_delete(H):
+  (H[0], H[-1]) = (H[-1], H[0])
+  min = H.pop()
+  p = 0
+  while p < len(H):
+    i = 2*p + 1                            # i = the left child of p
+    if i >= len(H): break                  # if there is no left child
+    if i + 1 < len(H) and H[i + 1] < H[i]: # if the right child is smaller
+      i = i + 1                            #   i = the right child of p
+    if H[p] < H[i]:                        # if the parent is smaller
+      break
+    (H[p], H[i]) = (H[i], H[p])
+    p = i
+  return min
 
-A = [94,65,39,75,56,93,55,21,38,33]
+A = [94,65,39,75,56,93,55,21,38,33,27]
 H = []
 for x in A:
   heap_insert(H, x)
 print(H)
+while H:
+  x = heap_delete(H)
+  print(x, H)
